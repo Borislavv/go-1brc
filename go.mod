6a4d6fe93844aa767d739106b7fd2e7281e1161c
Boris <glazunov2142@icloud.com>
2024-06-22 16:03:29 +0300

    feat: 1brc - 5.7seconds.

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Borislavv/go-1brc
+
+go 1.22.1
+
+require github.com/dolthub/swiss v0.2.1
+
+require github.com/dolthub/maphash v0.1.0 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"bytes"
+	"github.com/dolthub/swiss"
+	"io"
 	"os"
-	"runtime"
-	"runtime/pprof"
 	"strconv"
 	"sync"
-	"syscall"
 )
 
-const path = "/Users/admin/projects/rust/1brc-rust/measurements.txt"
+const (
+	filePath   = "/Users/admin/projects/rust/1brc-rust/measurements.txt"
+	bufferSize = 2048 * 2048
+	workersNum = 25
+)
 
 type Temperatures struct {
 	Station string
@@ -24,148 +27,166 @@ func NewTemperatures(min, mean, max float64, cnt int, station string) *Temperatu
 	return &Temperatures{Min: min, Mean: mean, Max: max, Cnt: cnt, Station: station}
 }
 
-type chunk struct {
-	data []byte
+type incompleteLine struct {
+	p       []byte // data
+	n       int    // chunk number
+	isFirst bool   // is first line? if not, then it's mean that it's the last
 }
 
 func main() {
-	f, err := os.Create(os.DevNull)
+	//pprofFile, err := os.Create("cpu.pprof")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer func() { _ = pprofFile.Close() }()
+	//
+	////pprofFile, _ := os.Open(os.DevNull)
+	////defer func() { _ = pprofFile.Close() }()
+	//_ = pprof.StartCPUProfile(pprofFile)
+	//defer pprof.StopCPUProfile()
+
+	f, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
 	}
 	defer func() { _ = f.Close() }()
 
-	// profiler is doing some optimizations, with it the code works faster
-	_ = pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
-
-	ff, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = ff.Close() }()
-
-	st, err := ff.Stat()
-	if err != nil {
-		panic(err)
-	}
-
-	bufferedFile, err := syscall.Mmap(int(ff.Fd()), 0, int(st.Size()), syscall.PROT_READ, syscall.MAP_PRIVATE)
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = syscall.Munmap(bufferedFile) }()
-
-	cpu := runtime.NumCPU()
-
-	chunksCh := make(chan *chunk, cpu)
-	go getChunks(chunksCh, bufferedFile, st.Size(), cpu)
-
+	mu := new(sync.Mutex)
 	wg := &sync.WaitGroup{}
 
-	resCh := make(chan map[uint64]*Temperatures, cpu)
-	wg.Add(1)
-	go processChunks(wg, chunksCh, resCh)
+	resultMap := swiss.NewMap[uint64, *Temperatures](500)
 
-	resultsMap := make(map[uint64]*Temperatures, 500)
+	incompleteLinesCh := make(chan incompleteLine, workersNum-2)
+	go processIncompleteLine(incompleteLinesCh, resultMap)
+
+	resCh := make(chan *swiss.Map[uint64, *Temperatures], workersNum)
 	wg.Add(1)
-	go aggregateResults(wg, resCh, resultsMap)
+	go processChunks(wg, mu, resCh, incompleteLinesCh)
+
+	wg.Add(1)
+	go aggregateResults(wg, resCh, resultMap)
 
 	wg.Wait()
 
-	printResult(resultsMap)
+	printResult(resultMap)
 }
 
-func getChunks(chunksCh chan<- *chunk, bufferedFile []byte, filesize int64, cpu int) {
-	defer close(chunksCh)
-
-	chunksize := filesize / int64(cpu)
-
-	offset := int64(0)
-	for i := 0; i < cpu; i++ {
-		if i == cpu-1 {
-			if offset < filesize {
-				chunksCh <- &chunk{data: bufferedFile[offset:]}
-			}
-			break
-		}
-
-		seekOffset := offset + chunksize
-		if seekOffset > filesize {
-			seekOffset = filesize
-		}
-
-		lastNewlineIndex := bytes.LastIndexByte(bufferedFile[offset:seekOffset], '\n')
-		nextOffset := offset + int64(lastNewlineIndex) + 1
-
-		chunksCh <- &chunk{data: bufferedFile[offset:nextOffset]}
-
-		offset = nextOffset
-	}
-}
-
-func processChunks(wg *sync.WaitGroup, chunksCh <-chan *chunk, resCh chan<- map[uint64]*Temperatures) {
+func processChunks(
+	mwg *sync.WaitGroup,
+	mu *sync.Mutex,
+	resMapsCh chan<- *swiss.Map[uint64, *Temperatures],
+	incompleteLinesCh chan<- incompleteLine,
+) {
 	defer func() {
-		wg.Done()
-		close(resCh)
+		close(resMapsCh)
+		close(incompleteLinesCh)
+		mwg.Done()
 	}()
 
-	wgg := &sync.WaitGroup{}
-	defer wgg.Wait()
-	for chunkBuffer := range chunksCh {
-		wgg.Add(1)
-		go func(chunkData *chunk) {
-			defer wgg.Done()
+	f, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = f.Close() }()
 
-			resMap := make(map[uint64]*Temperatures, 100)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
 
-			s := 0
-			l := len(chunkData.data)
-			for i := 0; i < l; i++ {
-				if chunkData.data[i] == '\n' {
-					var station []byte
-					temperature := 0.00
-					for j := 0; j < len(chunkData.data[s:i]); j++ {
-						if chunkData.data[s:i][j] == ';' {
-							station = chunkData.data[s:i][:j]
-							temperature = parseFloat(chunkData.data[s:i][j+1:])
-						}
-					}
+	for chunk := 0; chunk < workersNum; chunk++ {
+		wg.Add(1)
+		go func(chunk int) {
+			defer wg.Done()
 
-					h := hash(station)
+			resMap := swiss.NewMap[uint64, *Temperatures](100)
 
-					temps, found := resMap[h]
-					if !found {
-						resMap[h] = NewTemperatures(temperature, temperature, temperature, 1, string(station))
+			buffer := make([]byte, bufferSize)
+			for {
+				mu.Lock()
+				_, err = f.Read(buffer)
+				mu.Unlock()
+				if err != nil {
+					if err == io.EOF {
+						break
 					} else {
-						temps.Cnt++
-						temps.Mean += temperature
-
-						if temperature < temps.Min {
-							temps.Min = temperature
-						} else if temperature > temps.Max {
-							temps.Max = temperature
-						}
+						panic(err)
 					}
+				}
 
-					s = i + 1
+				// skip the last incomplete line and pass it to appropriate handler
+				lastCorrectIdx := len(buffer)
+				if chunk != 0 {
+					lastCorrectIdx = bytes.LastIndexByte(buffer, '\n') + 1
+					incompleteLinesCh <- incompleteLine{
+						p:       buffer[lastCorrectIdx:],
+						n:       chunk,
+						isFirst: false,
+					}
+				}
+
+				firstCorrectIdx := 0
+				if chunk != workersNum-1 {
+					firstCorrectIdx = bytes.IndexByte(buffer, '\n')
+					incompleteLinesCh <- incompleteLine{
+						p:       buffer[:firstCorrectIdx],
+						n:       chunk,
+						isFirst: true,
+					}
+				}
+
+				buffer = buffer[firstCorrectIdx:lastCorrectIdx]
+
+				s := 0
+				for i := 0; i < len(buffer); i++ {
+					if buffer[i] == '\n' {
+						var station []byte
+						temperature := 0.00
+						for j := 0; j < len(buffer[s:i]); j++ {
+							if buffer[s:i][j] == ';' {
+								station = buffer[s:i][:j]
+								temperature = parseFloat(buffer[s:i][j+1:])
+							}
+						}
+
+						h := hash(station)
+
+						temps, found := resMap.Get(h)
+						if !found {
+							resMap.Put(h, NewTemperatures(temperature, temperature, temperature, 1, string(station)))
+						} else {
+							temps.Cnt++
+							temps.Mean += temperature
+
+							if temperature < temps.Min {
+								temps.Min = temperature
+							} else if temperature > temps.Max {
+								temps.Max = temperature
+							}
+						}
+
+						s = i + 1
+					}
 				}
 			}
 
-			resCh <- resMap
-		}(chunkBuffer)
+			resMapsCh <- resMap
+		}(chunk)
 	}
 }
 
-func aggregateResults(wg *sync.WaitGroup, resultsCh <-chan map[uint64]*Temperatures, resultsMap map[uint64]*Temperatures) {
+func processIncompleteLine(incompleteLinesCh <-chan incompleteLine, resultsMap *swiss.Map[uint64, *Temperatures]) {
+	for _ = range incompleteLinesCh {
+		//fmt.Printf("data: %v, n: %d, isFirst: %v \n", string(line.p), line.n, line.isFirst)
+	}
+}
+
+func aggregateResults(wg *sync.WaitGroup, resultsCh <-chan *swiss.Map[uint64, *Temperatures], resultsMap *swiss.Map[uint64, *Temperatures]) {
 	defer wg.Done()
 
 	for resMap := range resultsCh {
-		for h, stat := range resMap {
-
-			temps, found := resultsMap[h]
+		resMap.Iter(func(h uint64, stat *Temperatures) (stop bool) {
+			temps, found := resultsMap.Get(h)
 			if !found {
-				resultsMap[h] = NewTemperatures(stat.Min, stat.Mean, stat.Max, 1, stat.Station)
+				resultsMap.Put(h, NewTemperatures(stat.Min, stat.Mean, stat.Max, 1, stat.Station))
 			} else {
 				temps.Cnt++
 				temps.Mean += stat.Mean
@@ -176,15 +197,16 @@ func aggregateResults(wg *sync.WaitGroup, resultsCh <-chan map[uint64]*Temperatu
 					temps.Max = stat.Max
 				}
 			}
-		}
+			return false
+		})
 	}
 }
 
-func printResult(resultsMap map[uint64]*Temperatures) {
+func printResult(resultsMap *swiss.Map[uint64, *Temperatures]) {
 	i := 0
-	l := len(resultsMap)
+	l := resultsMap.Count()
 	_, _ = os.Stdout.WriteString("{")
-	for _, stat := range resultsMap {
+	resultsMap.Iter(func(_ uint64, stat *Temperatures) (stop bool) {
 		_, _ = os.Stdout.WriteString(
 			stat.Station + "=" + strconv.FormatFloat(stat.Min, 'f', 2, 64) +
 				"/" + strconv.FormatFloat(stat.Mean/float64(stat.Cnt), 'f', 2, 64) + "/" +
@@ -194,7 +216,8 @@ func printResult(resultsMap map[uint64]*Temperatures) {
 			_, _ = os.Stdout.WriteString(", ")
 		}
 		i++
-	}
+		return false
+	})
 	_, _ = os.Stdout.WriteString("}\n")
 }
 
